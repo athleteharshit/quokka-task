c298f30109eae61a3520fcfd8a7e441664a2c1d5
Harshit gupta <guptaharshit545@gmail.com>
2023-04-12 18:18:08 +0530

    adding functionality done

--- src/components/member/member.tsx
+++ src/components/member/member.tsx
@@ -1,11 +1,26 @@
 import { useState } from 'react';
 import { Button } from 'flowbite-react';
+
 import TableContainer, { MemberList } from '../table-container/table.container';
+import getUniqueId from '../../utils/common.fun';
+import ModalContainer from '../modal/modal.container';
 
 function Member() {
+  const [modal, setModal] = useState(false);
+  const [member, setMember] = useState({
+    memberId: '',
+    name: '',
+    email: '',
+    address: '',
+    organization: '',
+    designation: '',
+    contact: '',
+    status: false,
+    action: 'Add',
+  } as MemberList & { action: 'Edit' | 'Add' });
   const [memberList, setMemberList] = useState<MemberList[]>([
     {
-      memberId: '',
+      memberId: getUniqueId(),
       name: 'harshit',
       email: 'gupta545@gmail.com',
       address: 'kanpur',
@@ -16,13 +31,31 @@ function Member() {
     },
   ]);
 
+  const handleModalOpen = () => {
+    setModal(true);
+  };
+
+  const handleModalClose = () => {
+    setModal(false);
+  };
+
+  const handleAddMember = (user: MemberList & { action: 'Edit' | 'Add' }) => {
+    setMemberList([...memberList, user]);
+  };
+
   return (
     <div className="p-4">
       <div className="flex items-center justify-between">
         <h1>Member List</h1>
-        <Button>Add a member</Button>
+        <Button onClick={handleModalOpen}>Add a member</Button>
       </div>
       <TableContainer list={memberList} />
+      <ModalContainer
+        show={modal}
+        onClose={handleModalClose}
+        member={member}
+        addMember={handleAddMember}
+      />
     </div>
   );
 }
---
+++ src/components/modal/modal.container.tsx
@@ -0,0 +1,138 @@
+import { Modal, Button, TextInput } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { MemberList } from '../table-container/table.container';
+import getUniqueId from '../../utils/common.fun';
+
+type ModalContainerProps = {
+  show: boolean;
+  onClose: () => void;
+  member: MemberList & { action: 'Edit' | 'Add' };
+  addMember: (user: MemberList & { action: 'Edit' | 'Add' }) => void;
+};
+
+function ModalContainer({
+  show,
+  onClose,
+  member,
+  addMember,
+}: ModalContainerProps) {
+  const [user, setUser] = useState({
+    memberId: '',
+    name: '',
+    email: '',
+    address: '',
+    organization: '',
+    designation: '',
+    contact: '',
+    status: false,
+    action: 'Add',
+  } as MemberList & { action: 'Edit' | 'Add' });
+
+  useEffect(() => {
+    if (member.memberId && member.action === 'Edit') {
+      setUser({ ...member });
+    }
+  }, [member]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (eve: React.FormEvent<HTMLFormElement>) => {
+    eve.preventDefault();
+    addMember({
+      ...user,
+      memberId: getUniqueId(),
+    });
+    onClose();
+  };
+
+  return (
+    <Modal show={show} onClose={onClose}>
+      <form onSubmit={handleSubmit}>
+        <Modal.Header>{user.action}</Modal.Header>
+        <Modal.Body>
+          {user.action === 'Edit' && (
+            <TextInput
+              value={user.memberId}
+              onChange={handleChange}
+              type="text"
+              name="memberId"
+              placeholder="Member ID"
+              readOnly
+            />
+          )}
+          <div className="my-2 flex gap-2">
+            <TextInput
+              className="basis-1/2"
+              value={user.name}
+              onChange={handleChange}
+              type="text"
+              name="name"
+              placeholder="Name"
+              required
+            />
+            <TextInput
+              className="basis-1/2"
+              value={user.email}
+              onChange={handleChange}
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <div className="my-2 flex gap-2">
+            <TextInput
+              className="basis-1/2"
+              value={user.address}
+              onChange={handleChange}
+              type="text"
+              name="address"
+              placeholder="Address"
+              required
+            />
+            <TextInput
+              className="basis-1/2"
+              value={user.organization}
+              onChange={handleChange}
+              type="text"
+              name="organization"
+              placeholder="Organization"
+              required
+            />
+          </div>
+          <div className="my-2 flex gap-2">
+            <TextInput
+              className="basis-1/2"
+              value={user.designation}
+              onChange={handleChange}
+              type="text"
+              name="designation"
+              placeholder="Designation"
+              required
+            />
+            <TextInput
+              className="basis-1/2"
+              value={user.contact}
+              onChange={handleChange}
+              type="text"
+              name="contact"
+              placeholder="Contact"
+              required
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button type="submit">Add</Button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  );
+}
+
+export default ModalContainer;
